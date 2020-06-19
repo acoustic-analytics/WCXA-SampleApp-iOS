e62435b78bfba4939318f17a3c6e17b42ff29009
Shridhar Damale <shridhar.damale@acoustic.co>
2020-06-19 12:41:16 -0700

    Updated comments for application developers

--- CXA/NSString+TealeafCryptography.h
+++ CXA/NSString+TealeafCryptography.h
@@ -6,6 +6,10 @@
 //  Copyright © 2020 IBM. All rights reserved.
 //
 
+/*
+ This file is provided as sample only. You should implement your own categories to NSData and NSString for encryption-decryption as per your security policy. You do not have to have these methods or categories. This is optional. If these methods are implemented in your application, Tealeaf SDK  would call these APIs before saving data to local storage. If you choose to implement these; please test the methods properly; make sure there is a faithful encryption and decryption of the JSON string passed in. SDK relies on your application's NSString ecryption decryption methods. SDK itself does not encrypt, decrypt or validate if these operations are successful. Hence carefully implement and test.
+ */
+
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
