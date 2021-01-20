441325e283d31545c72ec2c184a3510299cefe07
Omar Hernandez <omar.hernandez@acoustic.co>
2021-01-20 12:06:38 -0800

    Update copyright.

--- CXA/CartCheckOutTableViewCell.m
+++ CXA/CartCheckOutTableViewCell.m
@@ -3,7 +3,13 @@
 //  CXA
 //
 //  Created by Chanikya on 10/26/16.
-//  Copyright © 2016 IBM. All rights reserved.
+//  Copyright (C) 2016 Acoustic, L.P. All rights reserved.
+//
+//  NOTICE: This file contains material that is confidential and proprietary to
+//  Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
+//  industrial property rights of Acoustic, L.P. except as may be provided in an agreement with
+//  Acoustic, L.P. Any unauthorized copying or distribution of content from this file is
+//  prohibited.
 //
 
 #import "CartCheckOutTableViewCell.h"
--- CXA/EndUserConsentViewController.h
+++ CXA/EndUserConsentViewController.h
@@ -3,7 +3,13 @@
 //  CXA
 //
 //  Created by Shridhar Damale on 2/12/19.
-//  Copyright © 2019 IBM. All rights reserved.
+//  Copyright (C) 2019 Acoustic, L.P. All rights reserved.
+//
+//  NOTICE: This file contains material that is confidential and proprietary to
+//  Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
+//  industrial property rights of Acoustic, L.P. except as may be provided in an agreement with
+//  Acoustic, L.P. Any unauthorized copying or distribution of content from this file is
+//  prohibited.
 //
 
 #import <UIKit/UIKit.h>
