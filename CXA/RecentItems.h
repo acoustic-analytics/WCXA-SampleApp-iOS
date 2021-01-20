15471daf0ef84b4ae1f019dc55487cf9bfa98a13
Omar Hernandez <omar.hernandez@acoustic.co>
2021-01-20 12:08:16 -0800

    Update copyright.

--- CXA/RecentItems.h
+++ CXA/RecentItems.h
@@ -3,7 +3,13 @@
 //  CXA
 //
 //  Created by Chanikya on 3/29/17.
-//  Copyright © 2017 IBM. All rights reserved.
+//  Copyright (C) 2017 Acoustic, L.P. All rights reserved.
+//
+//  NOTICE: This file contains material that is confidential and proprietary to
+//  Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
+//  industrial property rights of Acoustic, L.P. except as may be provided in an agreement with
+//  Acoustic, L.P. Any unauthorized copying or distribution of content from this file is
+//  prohibited.
 //
 
 #import <Realm/Realm.h>
